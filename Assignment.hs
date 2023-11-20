8736f2d69772fbf0575aca0a931760b71e8fd84b
Razvan Gorea <razvan.gorea2@mail.dcu.ie>
2023-11-20 17:37:34 +0000

    Checkpoint

--- Assignment.hs
+++ Assignment.hs
@@ -1,8 +1,13 @@
+-- Tree Data Structure
+-- A 2-3 Tree can either be empty(nil), a 2-node(1 root and two children) , or a 3-node(two roots and three children) 
 data Tree t = 
   Nil
   | Node2 t (Tree t) (Tree t)
   | Node3 t t (Tree t) (Tree t) (Tree t) deriving (Eq, Show, Ord)
 
+-- Add function that adds a node to the 2-3 tree
+-- If the tree is empty, then the node is added as the root (in this case, the root is a 2-node)
+-- Otherwise it goes through the tree, recursively left most as possible, until it finds the right place to add the node
 add :: Ord t => t -> Tree t -> Tree t
 add a Nil = Node2 a Nil Nil
 add x (Node2 a left right)  
@@ -13,7 +18,8 @@ add x (Node3 a b left middle right)
    | x > a && x < b = Node3 a b left (add x middle) right
    | otherwise = Node3 a b left middle (add x right)
 
-
+-- Member function that checks if a node is in the 2-3 tree
+-- The member function recursively goes through the tree, left most as possible, until it finds the node
 member :: (Ord t) => t -> Tree t -> Bool
 member _ Nil = False
 member x (Node2 a left right)
@@ -26,3 +32,32 @@ member x (Node3 a b left middle right)
    | x > a && x < b = member x middle
    | otherwise = member x right
 
+-- Height function that returns the height of the 2-3 tree
+height :: Tree t -> Int
+height Nil = -1
+height (Node2 _ left right) = 1 + max (height left) (height right)
+height (Node3 _ _ left middle right) = 1 + max (height left) (max(height middle) (height right))
+
+-- makeTree function that takes a list and returns a 2-3 tree
+-- The makeTree function recursively goes through a given list, extracting the head through each pass and adding the head to the tree
+-- The add function is used to add the head to the tree
+makeTree :: Ord t => [t] -> Tree t
+makeTree [] = Nil
+makeTree (x:xs) = add x (makeTree xs)
+
+-- Unsure if correct???
+prettyPrint :: Show t => Tree t -> String
+prettyPrint Nil = ""
+prettyPrint (Node2 a left right) = prettyPrint left ++ show a ++ prettyPrint right
+prettyPrint (Node3 a b left middle right) = prettyPrint left ++ show a ++ prettyPrint middle ++ show b ++ prettyPrint right
+
+{-
+
+Questions:
+
+1. What happens when you use the add method to add a node to an empty tree?
+2. If the height method is presented with an empty tree, should it return 0, -1 or 1?
+3. How to pretty print?
+4. Add Method? is less that or equal to correct? or should it just be less than?
+
+-}
